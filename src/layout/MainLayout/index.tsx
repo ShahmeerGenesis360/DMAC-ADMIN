bb9df3a17b540bef442f58f1380951e0c7b00c89
ShahmeerGenesis360 <shahmeer@genesis360.co>
2025-01-02 01:10:03 +0500

    wrap in Outlet

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -1,15 +1,13 @@
 import { Flex } from 'antd';
 import React from 'react'
 import { Sidebar } from '../../components'
-// import { Outlet } from 'react-router-dom';
-import { TopWallet } from '../../pages';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <Flex style={{ overflow: 'hidden' }}>
             <Sidebar />
-            <TopWallet />
-            {/* <Outlet /> */}
+            <Outlet />
         </Flex>
     )
 }
