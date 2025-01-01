9e1e3e1a0ba6ab1d515362217ad6dbf1289bee5f
ShahmeerGenesis360 <shahmeer@genesis360.co>
2025-01-02 02:23:43 +0500

    button component added

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import ImageBox from "./image";
 import TextField from "./textField";
 import Table from "./table";
 import Pagination from "./pagination";
+import Button from "./button";
 
-export { Sidebar, ImageBox, TextField, Table, Pagination };
+export { Sidebar, ImageBox, TextField, Table, Pagination, Button };
