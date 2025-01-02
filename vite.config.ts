99f2890d9815bc34d9a1157d5e6bf2c82acbdff8
ShahmeerGenesis360 <shahmeer@genesis360.co>
2025-01-03 03:36:11 +0500

    fixed issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use './' for relative paths if deploying to a subdirectory
+  base: '/', // Use './' for relative paths if deploying to a subdirectory
   build: {
     outDir: 'dist', // Ensure output directory is 'dist'
     emptyOutDir: true
