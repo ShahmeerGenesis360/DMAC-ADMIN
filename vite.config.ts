03c86758f105965647d48b23509da33d7377d182
ShahmeerGenesis360 <shahmeer@genesis360.co>
2025-01-03 03:29:23 +0500

    set default

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ensure the output directory is `dist`, which is default for Vite
-  }
-});
+})
