e86c09a381052ae1b49626af92c3c867e59fcf7b
decentralized <abhishek@wifidabba.com>
2025-01-08 10:21:32 +0530

    createIndex Requirements

---
+++ src/constants/blockchain.js
@@ -0,0 +1,6 @@
+export const PYTH_NETWORK_PROGRAM_ID =
+  "7UVimffxr9ow1uXYxsr4LHAcV58mLzhmwaeKvJ1pjLiE";
+export const NEXT_PUBLIC_PROGRAM_ID =
+  "5Pd13rYQh5GRUVhvDAiwkKi2v22AbD9k3nBKQfMneo76";
+export const NEXT_PUBLIC_ADMIN_PK =
+  "5uXeNyrP4ijmpkkjgytupF2UsPNwbANMSgWvakiEr3UEnU2t7jmJAPFWTSKbH5gEWZhUj43c6KQ6Hwhx8SronhMW";
